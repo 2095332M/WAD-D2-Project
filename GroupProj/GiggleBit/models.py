4c3f56ec9ca83805a292c7f84b9a8ee46cbd657f
Dylan <stevensondylan13@googlemail.com>
2015-03-07 21:04:25 +0000

    forgot to import slugafie

--- GroupProj/GiggleBit/models.py
+++ GroupProj/GiggleBit/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 class userprofile(models.models): #additional user stuff
