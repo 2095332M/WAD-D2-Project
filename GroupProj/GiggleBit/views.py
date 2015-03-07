6887497342816d201c26b2997e612e5b34a8e042
Kelvin Fowler <2083905f@student.gla.ac.uk>
2015-03-07 17:24:46 +0000

    backbone views added

--- GroupProj/GiggleBit/views.py
+++ GroupProj/GiggleBit/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#CHAPTER 19 of rango book is basically essential for our app.
+
+
+#super sick home page
+def index(request):
+    return HttpResponse("IMPLEMENT ME")
+
+#similar/same as /category/ in rango
+def tilde(request):
+    return HttpResponse("IMPLEMENT ME")
+
+
+#like add_page but needs to deal with multiple ~'s per image
+#and is accesible from all other pages
+def add_image(request): 
+    return HttpResponse("IMPLEMENT ME")
+
+
+#image/unique_identifier/ one for each image, need random id creator
+def image(request):
+    return HttpResponse("IMPLEMENT ME")
+
+
+#REDIRECT PAGE for each image to track page views for sorting/displaying algorithms
+#could be too hard within time frame to do algorithms but this view itself is ok
+#basically exactly the same to rango's track_url
+def track_url(request):
+    return HttpResponse("IMPLEMENT ME")
+
+
+#Probabaly is necessary to return results of searching images
+def search(request):
+    return HttpResponse("IMPLEMENT ME")
+
+#About Page
+def about(request):
+    return HttpRespone("Produced by Stephen McMorran, Dillon Stevenson, Stuart Mackle and Kelvin Fowler")
+
+
